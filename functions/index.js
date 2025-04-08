79386977904ab31d2b974df2a82a778447beadc7
romulo.nunes <rnunes@viceri.com.br>
2025-04-08 10:50:14 -0300

    primeiro

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,16 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
 
 admin.initializeApp();
 const db = admin.firestore();
 
-exports.confirmarPresenca = functions.https.onRequest(async (req, res) => {
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json()); // <-- necessário para req.body funcionar
+
+app.post("/", async (req, res) => {
   const { nome, presenca } = req.body;
 
   if (!nome || !presenca) {
@@ -27,3 +33,5 @@ exports.confirmarPresenca = functions.https.onRequest(async (req, res) => {
     return res.status(500).json({ message: "Erro ao confirmar presença." });
   }
 });
+
+exports.confirmarPresenca = functions.https.onRequest(app);
