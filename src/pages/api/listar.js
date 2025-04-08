dd865fc1bf47b41c12c0a7e9d3fdb14450a0bb13
romulo.nunes <rnunes@viceri.com.br>
2025-04-08 16:57:29 -0300

    priru

--- src/pages/api/listar.js
+++ src/pages/api/listar.js
@@ -1,34 +1,17 @@
-// Importa os módulos necessários do Firebase
+// firebase-config.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
-// Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAnhrtpLXEGxS6eZdTU1I9Q8WeKNIljo34",
   authDomain: "aniversario-melinda.firebaseapp.com",
   projectId: "aniversario-melinda",
-  storageBucket: "aniversario-melinda.appspot.com", // 🔹 Corrigido!
+  storageBucket: "aniversario-melinda.appspot.com",
   messagingSenderId: "573530545668",
   appId: "1:573530545668:web:f4adc9088d286ccc6f9f51",
-  measurementId: "G-HDMZPBY92T",
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// Inicializa o Firestore
 const db = getFirestore(app);
 
-// Função para lidar com a requisição e retornar os dados da coleção "presencas"
-export default async function handler(req, res) {
-  try {
-    const snapshot = await getDocs(collection(db, "presencas"));
-    const lista = snapshot.docs.map((doc) => doc.data());
-    res.status(200).json(lista);
-  } catch (error) {
-    console.error("Erro ao buscar dados do Firestore:", error);
-    res.status(500).json({ error: "Erro interno do servidor" });
-  }
-}
+export { db };
