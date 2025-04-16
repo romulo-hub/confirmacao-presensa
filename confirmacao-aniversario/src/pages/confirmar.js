ac6fa501681a737de7a3e655f782b1a38cab32bd
romulo.nunes <rnunes@viceri.com.br>
2025-04-16 18:07:59 -0300

    fasfsa

--- confirmacao-aniversario/src/pages/confirmar.js
+++ confirmacao-aniversario/src/pages/confirmar.js
@@ -1,7 +1,7 @@
 export default function Confirmar() {
   return (
-    <div className="min-h-screen bg-white flex items-center justify-center">
-      <h1 className="text-2xl font-bold text-purple-700">
+    <div classname="min-h-screen bg-white flex items-center justify-center">
+      <h1 classname="text-2xl font-bold text-purple-700">
         Presença confirmada! 🎉
       </h1>
     </div>
